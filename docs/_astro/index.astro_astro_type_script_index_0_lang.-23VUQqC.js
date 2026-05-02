6f70d8529b0b6225f5ac82695cae5e029eadc0d4
Eeshan Srivastava <77134595+eeshansrivastava89@users.noreply.github.com>
2026-05-02 12:05:26 -0700

    updated demo

---
+++ docs/_astro/index.astro_astro_type_script_index_0_lang.-23VUQqC.js
@@ -0,0 +1,1 @@
+import"./linguistics.astro_astro_type_script_index_0_lang.IkwlIQ2x.js";import"./shared.C-Odfgc7.js";
--- docs/_astro/index.astro_astro_type_script_index_0_lang.Du6VZmDf.js
+++
@@ -1,1 +0,0 @@
-import"./linguistics.astro_astro_type_script_index_0_lang.Dmp41JKe.js";import"./shared.BDsZ43uh.js";
